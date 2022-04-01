49d909804b909f96afa9e9d29582aa99db4a19ff
sneha-chillarge123 <95573561+sneha-chillarge123@users.noreply.github.com>
2022-04-01 15:45:22 +0530

    Add files via upload

---
+++ quiz_game_login.js
@@ -0,0 +1,7 @@
+function adduser(){
+    var player1_name=document.getElementById("player_1_name_input").value;
+    var player2_name=document.getElementById("player_2_name_input").value;
+    localStorage.setItem("player1_name" , player1_name);
+    localStorage.setItem("player2_name" , player2_name);
+    window.location="quiz_game_page.html";
+}
